90b32df129b79e205a84c56e5cbf49f1bc26e094
Rafael Massoni <Rafael.massoni4@gmail.com>
2020-06-07 18:09:17 +0000

    cors

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const database = require('./database');
 
+const cors = require('cors');
+
 const server = express();
 
+
+server.use(cors());
 server.use(express.json());
 
 
